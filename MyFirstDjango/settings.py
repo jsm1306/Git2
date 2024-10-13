0b948f55e2f05b1d258221ffbfae8c1189d4d560
jsm1306 <sindhu.j1729@gmail.com>
2024-10-13 19:08:15 +0530

    secondexp

--- MyFirstDjango/settings.py
+++ MyFirstDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calc',
+    'calc', #add your app name
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyFirstDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')], #add templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- calc/views.py
+++ calc/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    return render(request,'home.html',{'name':'Sindhu'})
+    return render(request, 'home.html')
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    val3 = val1 + val2
+    return render(request, 'result.html', {'result': val3})
