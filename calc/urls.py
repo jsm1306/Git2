762809a3aa292b191e5fad4840ceb98cb660d07a
jsm1306 <sindhu.j1729@gmail.com>
2024-10-14 15:00:44 +0530

    final commit

--- calc/urls.py
+++ calc/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.home,name='home'),
-    path('add', views.add, name='add'),
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('products/', views.products, name='products'),
+    path('add',views.add, name='add') 
 ]
--- calc/views.py
+++ calc/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def products(request):
+    return render(request, 'products.html')
+
 def add(request):
-    val1 = int(request.POST['num1'])
-    val2 = int(request.POST['num2'])
-    val3 = val1 + val2
-    return render(request, 'result.html', {'result': val3})
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    val3=val1+val2
+    return render(request,'result.html',{'result':val3})
