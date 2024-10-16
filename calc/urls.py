93e629c00a1da5bf59b3743c32bb8fa1cea5bfb4
emythri <eelapantimythri@gmail.com>
2024-10-16 19:38:24 +0530

    14th exp

--- calc/urls.py
+++ calc/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('update_order/<str:pk>/',views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>/',views.deleteOrder, name='delete_order'),
     path('register/',views.registerPage, name='register'), 
+    path('login/',views.loginPage, name='login'), 
+    path('logout/',views.logoutPage, name='logout'), 
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required 
+from django.views.decorators.cache import cache_control 
 
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def home(request):
     return render(request, 'home.html')
 
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def dashboard(request):
     customers=Customer.objects.all()
     orders=Order.objects.all()
     return render(request,'dashboard.html',{'customers':customers,'orders':orders})
 
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def products(request):
     products=Product.objects.all()
     return render(request,'products.html',{'products':products})
 
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def add(request):
     val1=int(request.POST['num1'])
     val2=int(request.POST['num2'])
     val3=val1+val2
     return render(request,'result.html',{'result':val3})
+
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def customer(request, pk_test):
     customer=Customer.objects.get(id=pk_test)
     customers=Customer.objects.all()
@@ -26,7 +41,10 @@ def customer(request, pk_test):
     context={'customers':customers, 'cust':customer,'orders':orders,'ordcount':order_count}
     return render(request,'customer.html',context)
 
+
 from .forms import OrderForm
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def createOrder(request):
     form=OrderForm()
     if request.method=="POST":
@@ -37,8 +55,10 @@ def createOrder(request):
 
     context={'form':form}
     return render(request,'order_form.html',context)
-from django.contrib import messages
 
+from django.contrib import messages
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def updateOrder(request, pk):
 
     order=Order.objects.get(id=pk)
@@ -51,6 +71,9 @@ def updateOrder(request, pk):
     context={'form':form}
     return render(request,'order_form.html',context)
 
+
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def deleteOrder(request, pk): 
     order=Order.objects.get(id=pk) 
     if request.method=="POST": 
@@ -72,3 +95,26 @@ def registerPage(request):
             
     context={'form':form} 
     return render(request, 'register.html', context)
+
+def loginPage(request): 
+    if request.user.is_authenticated: 
+        return redirect('home') 
+    else: 
+        if request.method=='POST': 
+            username=request.POST.get('username') 
+            password=request.POST.get('password') 
+            print(username, password) 
+            user = authenticate(request, username=username, password=password) 
+            if user is not None: 
+                login(request, user) 
+                return redirect('home') 
+            else: 
+                messages.success(request,"Username or Password is incorrect") 
+        context={} 
+        return render(request,'login.html',context)
+    
+@login_required(login_url='login') 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True) 
+def logoutPage(request): 
+    logout(request) 
+    return redirect('login') 
