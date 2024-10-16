908a6f70d864b3901978058f9f885c2cfd333e61
jsm1306 <sindhu.j1729@gmail.com>
2024-10-16 11:31:32 +0530

    11th exp

--- calc/urls.py
+++ calc/urls.py
@@ -4,5 +4,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('products/', views.products, name='products'),
-    path('add',views.add, name='add') 
+    path('add',views.add, name='add'),
+    path('customer/<str:pk_test>/',views.customer, name='customer'),
+    path('create_order/',views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/',views.updateOrder, name='update_order'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 def home(request):
     return render(request, 'home.html')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    customers=Customer.objects.all()
+    orders=Order.objects.all()
+    return render(request,'dashboard.html',{'customers':customers,'orders':orders})
 
 def products(request):
-    return render(request, 'products.html')
+    products=Product.objects.all()
+    return render(request,'products.html',{'products':products})
 
 def add(request):
     val1=int(request.POST['num1'])
     val2=int(request.POST['num2'])
     val3=val1+val2
     return render(request,'result.html',{'result':val3})
+def customer(request, pk_test):
+    customer=Customer.objects.get(id=pk_test)
+    customers=Customer.objects.all()
+    orders=customer.order_set.all()
+    order_count=orders.count()
+    context={'customers':customers, 'cust':customer,'orders':orders,'ordcount':order_count}
+    return render(request,'customer.html',context)
+
+from .forms import OrderForm
+def createOrder(request):
+    form=OrderForm()
+    if request.method=="POST":
+        form=OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'form':form}
+    return render(request,'order_form.html',context)
+
+def updateOrder(request, pk):
+
+    order=Order.objects.get(id=pk)
+    form=OrderForm(instance=order)
+    if request.method=="POST":
+        form=OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context={'form':form}
+    return render(request,'order_form.html',context)
